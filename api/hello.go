262b32e3e86dbe5d44d4a225555fc2a26163d2fe
Armando Arce <armando.arce@gmail.com>
2020-10-30 15:51:39 -0600

    Update hello.go

--- api/hello.go
+++ api/hello.go
@@ -7,11 +7,10 @@ import (
 
 func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
 	return &events.APIGatewayProxyResponse{
-		StatusCode:        200,
-		Headers:           map[string]string{"Content-Type": "text/plain"},
-		MultiValueHeaders: http.Header{"Set-Cookie": {"Ding", "Ping"}},
-		Body:              "Hello, World!",
-		IsBase64Encoded:   false,
+		StatusCode:      200,
+		Headers:         map[string]string{"Content-Type": "text/plain"},
+		Body:            "Hello, World!",
+		IsBase64Encoded: false,
 	}, nil
 }
 
