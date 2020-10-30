d9cd9cb58fbc8f05850fbab50809e93817ba3ea7
Armando Arce <armando.arce@gmail.com>
2020-10-30 15:45:57 -0600

    Update hello.go

--- api/hello.go
+++ api/hello.go
@@ -1,19 +1,21 @@
 package main
 
 import (
-	"context"
-	"fmt"
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-type MyEvent struct {
-	Name string `json:"name"`
-}
-
-func HandleRequest(ctx context.Context, name MyEvent) (string, error) {
-	return fmt.Sprintf("Hello %s!", name.Name), nil
+func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	return &events.APIGatewayProxyResponse{
+		StatusCode:        200,
+		Headers:           map[string]string{"Content-Type": "text/plain"},
+		MultiValueHeaders: http.Header{"Set-Cookie": {"Ding", "Ping"}},
+		Body:              "Hello, World!",
+		IsBase64Encoded:   false,
+	}, nil
 }
 
 func main() {
-	lambda.Start(HandleRequest)
+	// Make the handler available for Remote Procedure Call by AWS Lambda
+	lambda.Start(handler)
 }
