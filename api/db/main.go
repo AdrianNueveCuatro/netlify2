7b983875b9738750cb7d0242ea6224b4ffad4e26
Armando Arce <armando.arce@gmail.com>
2020-10-30 20:36:22 -0600

    Update

--- api/db/main.go
+++ api/db/main.go
@@ -4,9 +4,11 @@ import (
 	"context"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"log"
+	"time"
+
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	"os"
 )
 
 // Stores a handle to the collection being used by the Lambda function
@@ -24,13 +26,14 @@ func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResp
 }
 
 func main() {
-	ctx := context.Background()
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI(os.Getenv("ATLAS_URI")))
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(
+		"mongodb+srv://dbUser:87rt45th67>@cluster0.swvad.mongodb.net/Books?retryWrites=true&w=majority",
+	))
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
-
-	defer client.Disconnect(ctx)
 
 	lambda.Start(handler)
 }
