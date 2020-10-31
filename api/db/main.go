977c70a6f8f25ea8840cb00a02bfc2c09425f25e
Armando Arce <armando.arce@gmail.com>
2020-10-30 20:40:07 -0600

    Update

--- api/db/main.go
+++ api/db/main.go
@@ -29,7 +29,7 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI(
-		"mongodb+srv://dbUser:87rt45th67>@cluster0.swvad.mongodb.net/Books?retryWrites=true&w=majority",
+		"mongodb+srv://dbUser:87rt45th67@cluster0.swvad.mongodb.net/Books?retryWrites=true&w=majority",
 	))
 	if err != nil {
 		log.Fatal(err)
